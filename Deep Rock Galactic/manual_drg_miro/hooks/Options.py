3687b24542a88cfaf2678c6d91f14581e89bf1fe
Miro-n1 <malte.erbe@web.de>
2024-05-15 23:37:23 +0200

    Version 1.10
    Added new game mode

--- Deep Rock Galactic/manual_drg_miro/hooks/Options.py
+++ Deep Rock Galactic/manual_drg_miro/hooks/Options.py
@@ -26,7 +26,14 @@ from ..Helpers import is_option_enabled, get_option_value
 # To add an option, use the before_options_defined hook below and something like this:
 #   options["total_characters_to_win_with"] = TotalCharactersToWinWith
 
-class ApAssignments(Range):
+class Mode(Toggle):
+    """Choose the game mode.
+    Short (True): 4-8 assignments with weapon and equipment upgrades for difficulty logic. Skill based logic skips.
+    Long (False): 4-32 missions without difficulty logic. Items unlock missions, similar to muse dash songs."""
+    display_name = "Game mode short"
+    default = True
+
+class AssignmentCount(Range):
     """Choose the number of different assignments to complete."""
     display_name = "Number of AP assignments"
     range_start = 4
@@ -41,11 +48,103 @@ class NitraRemaining(Toggle):
     """Unlock checks with amount of Nitra remaining at the end of the mission, instead of with the amount of Nitra mined (resupplies called)."""
     display_name = "Remaining Nitra Checks"
 
+class MissionCount(Range):
+    """Choose the number of missions in the pool. Complete pool: 8 mission types * 4 classes = 32."""
+    display_name = "Number of missions"
+    range_start = 4
+    range_end = 32
+    default = 16
+
+class StartingMissionCount(Range):
+    """Choose the number of starting missions."""
+    display_name = "Number of starting missions"
+    range_start = 0
+    range_end = 16
+    default = 4
+
+class long_mission_completions_to_win(Range):
+    """Choose the number of mission completions required for victory."""
+    display_name = "Number of missions to win"
+    range_start = 4
+    range_end = 32
+    default = 16
+
+#class long_trophies_count(Range):
+    #"""Choose the number of trophies in the item pool."""
+    #display_name = "Number of trophies"
+    #range_start = 0
+    #range_end = 32
+    #default = 0
+
+#class long_trophies_to_win(Range):
+    #"""Choose the number of trophies required for victory."""
+    #display_name = "Number of trophies to win"
+    #range_start = 0
+    #range_end = 32
+    #default = 0
+
+class SecondaryObjectives(Toggle):
+    """Adds locations for 8 secondary objectives."""
+    display_name = "Secondary objectives"
+    default = True
+
+class Warnings(Toggle):
+    """Adds locations for 13 warning mutators."""
+    display_name = "Warnings"
+
+class Anomalies(Toggle):
+    """Adds locations for 8 anomaly mutators."""
+    display_name = "Anomalies"
+
+class DeepDiveCount(Range):
+    """Choose the number of deep dives to complete.
+    Deep dive seeds refresh weekly."""
+    display_name = "Number of deep dives"
+    range_start = 0
+    range_end = 4
+    default = 1
+
+class EliteDeepDiveCount(Range):
+    """Choose the number of elite deep dives to complete.
+    Deep dive seeds refresh weekly."""
+    display_name = "Number of elite deep dives"
+    range_start = 0
+    range_end = 4
+    default = 0
+
+class WeeklyAssignmentCount(Range):
+    """Choose the number of weekly priority assignments and weekly core hunts to complete.
+    The assignments refresh weekly, consider the planned async length."""
+    display_name = "Number of weekly assignments"
+    range_start = 0
+    range_end = 4
+    default = 0
+
+class SeasonChallengeCount(Range):
+    """Choose the number of season challenges to complete.
+    These refresh daily and require you to complete multiple missions each, 
+    so consider the planned async length. Recommended: not more than 1/4 of mission count."""
+    display_name = "Number of season challenges"
+    range_start = 0
+    range_end = 30
+    default = 0
+
 # This is called before any manual options are defined, in case you want to define your own with a clean slate or let Manual define over them
 def before_options_defined(options: dict) -> dict:
-    options["ap_assignments"] = ApAssignments
-    options["easy_start"] = EasyStart
-    options["nitra_remaining"] = NitraRemaining
+    options["game_mode_short"] = Mode
+    options["short_assignment_count"] = AssignmentCount
+    options["short_easy_start"] = EasyStart
+    options["short_nitra_remaining"] = NitraRemaining
+    options["long_mission_count"] = MissionCount
+    options["long_starting_mission_count"] = StartingMissionCount
+    options["long_mission_completions_to_win"] = long_mission_completions_to_win
+    options["long_secondary_objectives"] = SecondaryObjectives
+    options["long_warnings"] = Warnings
+    options["long_anomalies"] = Anomalies
+    options["long_deep_dive_count"] = DeepDiveCount
+    options["long_elite_deep_dive_count"] = EliteDeepDiveCount
+    options["long_weekly_assignment_count"] = WeeklyAssignmentCount
+    options["long_season_callenge_count"] = SeasonChallengeCount
     return options
 
 # This is called after any manual options are defined, in case you want to see what options are defined or want to modify the defined options
--- Deep Rock Galactic/manual_drg_miro/hooks/World.py
+++ Deep Rock Galactic/manual_drg_miro/hooks/World.py
@@ -36,22 +36,22 @@ def before_create_regions(world: World, multiworld: MultiWorld, player: int):
 
 # Called after regions and locations are created, in case you want to see or modify that information. Victory location is included.
 def after_create_regions(world: World, multiworld: MultiWorld, player: int):
-    # Use this hook to remove locations from the world
-    locationNamesToRemove = [] # List of location names
+
+    locationNamesToRemove = []
+    regions_to_remove = []
 
     # Add your code here to calculate which locations to remove
 
-    #Select which regions aren't needed and remove their locations
-    regions_to_remove = []
-    assignment_count = get_option_value(multiworld, player, "ap_assignments")
-    if assignment_count == 4:
-        regions_to_remove = ["Assignment 5", "Assignment 6", "Assignment 7", "Assignment 8"]
-    if assignment_count == 5:
-        regions_to_remove = ["Assignment 6", "Assignment 7", "Assignment 8"]
-    if assignment_count == 6:
-        regions_to_remove = ["Assignment 7", "Assignment 8"]
-    if assignment_count == 7:
-        regions_to_remove = ["Assignment 8"]
+    game_mode_short = get_option_value(multiworld, player, "game_mode_short")
+    
+    if game_mode_short:
+        #Select which regions aren't needed and remove their locations:
+        assignment_count = get_option_value(multiworld, player, "short_assignment_count")
+        while assignment_count < 8:
+            assignment_count += 1
+            regions_to_remove += [f"Assignment {assignment_count}"]
+        #print(regions_to_remove)
+
 
     for region in multiworld.regions:
         if region.player != player:
@@ -71,60 +71,177 @@ def after_create_regions(world: World, multiworld: MultiWorld, player: int):
     if hasattr(multiworld, "clear_location_cache"):
         multiworld.clear_location_cache()
 
+
 # The item pool before starting items are processed, in case you want to see the raw item pool at that stage
 def before_create_items_starting(item_pool: list, world: World, multiworld: MultiWorld, player: int) -> list:
-
-    #Randomly select AP assignments:
-    assignment_count = get_option_value(multiworld, player, "ap_assignments")
-    missions = ["Mining Expedition", "Egg Hunt", "On-site Refining", "Salvage Operation", "Point Extraction", "Escort Duty", "Elimination", "Industrial Sabotage"]
-    multiworld.random.shuffle(missions)
-    for itemName in missions[assignment_count:]: #this excludes the first assignment_count items in the list
-        delete_item = next(i for i in item_pool if i.name == itemName)
-        item_pool.remove(delete_item)
-
-    #Set assignment count item for logic:
-    assignment_counter = next(item for item in item_pool if item.name == f"AP{assignment_count}")
-    multiworld.push_precollected(assignment_counter)
-    items_to_remove = ["AP4","AP5","AP6","AP7","AP8"]
-    for itemName in items_to_remove:
-        delete_item = next(i for i in item_pool if i.name == itemName)
-        item_pool.remove(delete_item)
-
-    #Start inventory for easy start yaml option:
-    if get_option_value(multiworld, player, "easy_start"):
-        start_inventory_items = ["Traversal Tool 1/4", "Special Equipment 1/4"]
-        secondary_weapons = ["Secondary Weapon 1", "Secondary Weapon 2", "Secondary Weapon 3"]
-        multiworld.random.shuffle(secondary_weapons)
-        start_inventory_items += secondary_weapons[2:]
-        for itemName in start_inventory_items:
+    
+    locationNamesToRemove = []
+    itemNamesToRemove = []
+    regions_to_remove = []
+  
+    if get_option_value(multiworld, player, "game_mode_short"):
+        #Randomly select AP assignments:
+        assignment_count = get_option_value(multiworld, player, "short_assignment_count")
+        missions = ["Mining Expedition", "Egg Hunt", "On-site Refining", "Salvage Operation", "Point Extraction", "Escort Duty", "Elimination", "Industrial Sabotage"]
+        multiworld.random.shuffle(missions)
+        missions_to_delete = missions[assignment_count:] #Deletes missions starting from index assignment_count
+        for itemName in missions_to_delete: 
+            delete_item = next(i for i in item_pool if i.name == itemName)
+            item_pool.remove(delete_item)
+        
+        #Set assignment count item for logic:
+        assignment_counter = next(item for item in item_pool if item.name == f"Logic-only item for {assignment_count} assignments")
+        multiworld.push_precollected(assignment_counter)
+        items_to_remove = ["Logic-only item for 4 assignments","Logic-only item for 5 assignments","Logic-only item for 6 assignments","Logic-only item for 7 assignments","Logic-only item for 8 assignments"]
+        for itemName in items_to_remove:
+            delete_item = next(i for i in item_pool if i.name == itemName)
+            item_pool.remove(delete_item)
+        
+        #Start inventory and easy start yaml option:
+        start_inventory_items = [missions[0]] #last 0-4 are deleted above, the first is never deleted
+        classes = ["Driller", "Engineer", "Gunner", "Scout"]
+        multiworld.random.shuffle(classes)
+        start_inventory_items += [classes[0]]
+        primary_weapons = ["Primary Weapon 1", "Primary Weapon 2", "Primary Weapon 3"]
+        multiworld.random.shuffle(primary_weapons)
+        start_inventory_items += [primary_weapons[0]]
+        if get_option_value(multiworld, player, "short_easy_start"):
+            start_inventory_items += ["Traversal Tool 1/4", "Special Equipment 1/4"]
+            secondary_weapons = ["Secondary Weapon 1", "Secondary Weapon 2", "Secondary Weapon 3"]
+            multiworld.random.shuffle(secondary_weapons)
+            start_inventory_items += secondary_weapons[2:]
+        for itemName in start_inventory_items: #precollect items, then delete from item pool.
             start_inventory = next(item for item in item_pool if item.name == itemName)
             multiworld.push_precollected(start_inventory)
             item_pool.remove(start_inventory)
     
-    return item_pool
+    else:
+        #Randomly select missions, remove unneeded locations and items:
+        mission_count = get_option_value(multiworld, player, "long_mission_count")
+        missions = ['Mining Expedition with Driller', 'Mining Expedition with Engineer', 'Mining Expedition with Gunner', 'Mining Expedition with Scout', 'Egg Hunt with Driller', 'Egg Hunt with Engineer', 'Egg Hunt with Gunner', 'Egg Hunt with Scout', 'On-site Refining with Driller', 'On-site Refining with Engineer', 'On-site Refining with Gunner', 'On-site Refining with Scout', 'Salvage Operation with Driller', 'Salvage Operation with Engineer', 'Salvage Operation with Gunner', 'Salvage Operation with Scout', 'Point Extraction with Driller', 'Point Extraction with Engineer', 'Point Extraction with Gunner', 'Point Extraction with Scout', 'Escort Duty with Driller', 'Escort Duty with Engineer', 'Escort Duty with Gunner', 'Escort Duty with Scout', 'Elimination with Driller', 'Elimination with Engineer', 'Elimination with Gunner', 'Elimination with Scout', 'Industrial Sabotage with Driller', 'Industrial Sabotage with Engineer', 'Industrial Sabotage with Gunner', 'Industrial Sabotage with Scout']
+        multiworld.random.shuffle(missions)
+        if hasattr(multiworld, "re_gen_passthrough") == False:
+            missions_to_delete = missions[mission_count:] #Deletes missions starting from index mission_count
+            #print(f"missions_to_delete: {missions_to_delete}")
+            for itemName in missions_to_delete: 
+                delete_item = next(i for i in item_pool if i.name == itemName)
+                item_pool.remove(delete_item)
+                #print(f"delete_item: {delete_item}")
+                locationNamesToRemove += [f"{itemName} - Reward 1", f"{itemName} - Reward 2"]
+            #print(f"locationNamesToRemove: {locationNamesToRemove}")
+            missions = missions[:mission_count]
 
-# The item pool after starting items are processed but before filler is added, in case you want to see the raw item pool at that stage
-def before_create_items_filler(item_pool: list, world: World, multiworld: MultiWorld, player: int) -> list:
+        #Start inventory missions: 
+        starting_mission_count = get_option_value(multiworld, player, "long_starting_mission_count")
+        start_inventory_items = missions[:starting_mission_count]
+        for itemName in start_inventory_items: #precollect items, then delete from item pool.
+            start_inventory = next(item for item in item_pool if item.name == itemName)
+            multiworld.push_precollected(start_inventory)
+            item_pool.remove(start_inventory)
+        
+        #Set missions required for goal:
+        mission_completions_required = get_option_value(multiworld, player, "long_mission_completions_to_win")
+        if mission_completions_required > mission_count:
+            mission_completions_required = mission_count
+        victory_locations = ['Complete 4 missions', 'Complete 5 missions', 'Complete 6 missions', 'Complete 7 missions', 'Complete 8 missions', 'Complete 9 missions', 'Complete 10 missions', 'Complete 11 missions', 'Complete 12 missions', 'Complete 13 missions', 'Complete 14 missions', 'Complete 15 missions', 'Complete 16 missions', 'Complete 17 missions', 'Complete 18 missions', 'Complete 19 missions', 'Complete 20 missions', 'Complete 21 missions', 'Complete 22 missions', 'Complete 23 missions', 'Complete 24 missions', 'Complete 25 missions', 'Complete 26 missions', 'Complete 27 missions', 'Complete 28 missions', 'Complete 29 missions', 'Complete 30 missions', 'Complete 31 missions', 'Complete 32 missions']
+        victory_locations.remove(f'Complete {mission_completions_required} missions')
+        #print(victory_locations)
+        locationNamesToRemove += victory_locations
 
-    # Use this hook to remove items from the item pool
-    itemNamesToRemove = [] # List of item names
-    # Add your code here to calculate which items to remove.
-    #
-    # Because multiple copies of an item can exist, you need to add an item name
-    # to the list multiple times if you want to remove multiple copies of it.
+        #if trophies_required > trophies_total:
+        #    trophies_required = trophies_total
+        #index = trophies_required - 1
+        #del trophy_locations[index] #or trophy_locations.remove(f'Collect {trophies_required} trophies')
+        
+        #include secondary objectives, warnings and/or anomalies:
+        if get_option_value(multiworld, player, "long_secondary_objectives") == False:
+            itemNamesToRemove += world.item_name_groups["(L) Secondary objectives"]
+            locationNamesToRemove += world.location_name_groups["Secondary objectives"]
+        if get_option_value(multiworld, player, "long_warnings") == False:
+            itemNamesToRemove += world.item_name_groups["(L) Warning mutators"]
+            locationNamesToRemove += world.location_name_groups["Warning mutators"]
+        if get_option_value(multiworld, player, "long_anomalies") == False:
+            itemNamesToRemove += world.item_name_groups["(L) Anomaly mutators"]
+            locationNamesToRemove += world.location_name_groups["Anomaly mutators"]
+            
+        #include (elite) deep dives, weekly assignments and/or season challenges:
+        deep_dive_count = get_option_value(multiworld, player, "long_deep_dive_count")
+        itemNamesToRemove += ["Deep Dive"] * (4 - deep_dive_count)
+        while deep_dive_count < 4:
+            deep_dive_count += 1
+            regions_to_remove += [f"DD{deep_dive_count}"]
+        elite_deep_dive_count = get_option_value(multiworld, player, "long_elite_deep_dive_count")
+        itemNamesToRemove += ["Elite Deep Dive"] * (4 - elite_deep_dive_count)
+        while elite_deep_dive_count < 4:
+            elite_deep_dive_count += 1
+            regions_to_remove += [f"EDD{elite_deep_dive_count}"]
+        weekly_assignment_count = get_option_value(multiworld, player, "long_weekly_assignment_count")
+        itemNamesToRemove += ["Weekly Priority Assignment"] * (4 - weekly_assignment_count)
+        itemNamesToRemove += ["Weekly Core Hunt"] * (4 - weekly_assignment_count)
+        while weekly_assignment_count < 4:
+            weekly_assignment_count += 1
+            regions_to_remove += [f"WPA{weekly_assignment_count}"]
+            regions_to_remove += [f"WCH{weekly_assignment_count}"]
+        season_callenge_count = get_option_value(multiworld, player, "long_season_callenge_count")
+        itemNamesToRemove += ["Season challenge"] * (30 - season_callenge_count)
+        while season_callenge_count < 9:
+            season_callenge_count += 1
+            locationNamesToRemove += [f"Season Challenge 0{season_callenge_count}"]
+        while season_callenge_count < 30:
+            season_callenge_count += 1
+            locationNamesToRemove += [f"Season Challenge {season_callenge_count}"]
+
+    for region in multiworld.regions:
+        if region.player != player:
+            continue
+        #print(region.name)
+        if region.name in regions_to_remove:
+            #print("region removed")
+            for location in list(region.locations):
+                region.locations.remove(location)
+
+    #print(locationNamesToRemove)
+    for region in multiworld.regions:
+        if region.player == player:
+            for location in list(region.locations):
+                if location.name in locationNamesToRemove:
+                    region.locations.remove(location)
+                    #print(f"remove location: {location}")
+    if hasattr(multiworld, "clear_location_cache"):
+        multiworld.clear_location_cache()
+
+    #print(itemNamesToRemove)
+    #print(item_pool)
     for itemName in itemNamesToRemove:
         item = next(i for i in item_pool if i.name == itemName)
         item_pool.remove(item)
 
 
+    return item_pool
+
+# The item pool after starting items are processed but before filler is added, in case you want to see the raw item pool at that stage
+def before_create_items_filler(item_pool: list, world: World, multiworld: MultiWorld, player: int) -> list:
+
+    itemNamesToRemove = []
+
+    # Because multiple copies of an item can exist, you need to add an item name
+    # to the list multiple times if you want to remove multiple copies of it.
+
+    game_mode_short = get_option_value(multiworld, player, "game_mode_short")
+
     #Place a victory item on the last ap assignment:
-    assignment_count = get_option_value(multiworld, player, "ap_assignments")
-    last_assignment_location = f"Assignment {assignment_count}-3"
-    #print(last_assignment_location)
-    location = multiworld.get_location(last_assignment_location, player)
-    item_to_place = next(i for i in item_pool if i.name == "Assignments Complete!")
-    location.place_locked_item(item_to_place)
-    item_pool.remove(item_to_place)
+    if game_mode_short:
+        assignment_count = get_option_value(multiworld, player, "short_assignment_count")
+        last_assignment_location = f"Assignment {assignment_count}-3"
+        #print(last_assignment_location)
+        location = multiworld.get_location(last_assignment_location, player)
+        item_to_place = next(i for i in item_pool if i.name == "Assignments Complete!")
+        location.place_locked_item(item_to_place)
+        item_pool.remove(item_to_place)
+
+    for itemName in itemNamesToRemove:
+        item = next(i for i in item_pool if i.name == itemName)
+        item_pool.remove(item)
 
     return item_pool
 
